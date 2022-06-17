c1c559a37602f12c63cf80e68c4a2c006641dd8f
octoshrimpy <octavo.acapella@gmail.com>
2022-06-16 18:31:06 -0600

    changes

--- js/weather/weatherData.js
+++ js/weather/weatherData.js
@@ -2,62 +2,91 @@ let state
 let city
 
 class GrabWeather {
-    constructor(){
-        this.celsius 
-        this.celsiusHigh
-        this.celsiusLow
-        this.fahrenheit
-        this.fahrenheitHigh
-        this.fahrenheitLow 
-        this.weatherDescription
+  constructor() {
+    this.celsius
+    this.celsiusHigh
+    this.celsiusLow
+    this.fahrenheit
+    this.fahrenheitHigh
+    this.fahrenheitLow
+    this.weatherDescription
+    this.city
+    this.state
+  }
+  async fetchData(city = this.city, state = this.state) {
+    try {
+      let userLocation = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=c6bcf4305ed39d0237aa62d9f140df5e`
+      const res = await fetch(userLocation)
+      const data = await res.json()
+      // console.log(data)
+      return data
+    } catch (error) {
+      console.error(error)
     }
-    async fetchData(){
-        try {
-            let userLocation = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=c6bcf4305ed39d0237aa62d9f140df5e`
-            const res = await fetch(userLocation)
-            const data = await res.json()
-            console.log(data)
-            return data
-        } catch (error) {
-            console.log(error)
-        }
-    }
+  }
 
-    async makeCelsius(){
-        let allData = await this.fetchData()
-        this.celsius = (allData.main.temp -273.15).toFixed()
-        this.celsiusHigh = (allData.main.temp_max -273.15).toFixed()
-        this.celsiusLow = (allData.main.temp_min -273.15).toFixed()
-        console.log('celsius: ',this.celsius, this.celsiusHigh, this.celsiusLow)
-    }
+  makeCelsius(main) {
+    let celsius = (main.temp - 273.15).toFixed()
+    let celsiusHigh = (main.temp_max - 273.15).toFixed()
+    let celsiusLow = (main.temp_min - 273.15).toFixed()
+    // console.log("celsius: ", celsius, celsiusHigh, celsiusLow)
 
-    async makeFahrenheit(){
-        let allData = await this.fetchData()
-        this.fahrenheit = (((allData.main.temp - 273.15) * 9 / 5) + 32).toFixed()
-        this.fahrenheitHigh = (((allData.main.temp_max - 273.15) * 9 / 5) + 32).toFixed()
-        this.fahrenheitLow = (((allData.main.temp_min - 273.15) * 9 / 5) + 32).toFixed()
-        console.log('fahrenheit: ',this.fahrenheit, this.fahrenheitHigh, this.fahrenheitLow)
+    return {
+      current: celsius,
+      high: celsiusHigh,
+      low: celsiusLow
     }
+  }
 
-    async getWeatherDescription(){
-        let allData = await this.fetchData()
-        this.weatherDescription = allData.weather[0].main
-        console.log(this.weatherDescription)
+  makeFahrenheit(main) {
+    let fahrenheit = (((main.temp - 273.15) * 9) / 5 + 32).toFixed()
+    let fahrenheitHigh = (((main.temp_max - 273.15) * 9) / 5 + 32).toFixed()
+    let fahrenheitLow = (((main.temp_min - 273.15) * 9) / 5 + 32).toFixed()
+    // console.log("fahrenheit: ", fahrenheit, fahrenheitHigh, fahrenheitLow)
+
+    return {
+      current: fahrenheit,
+      high: fahrenheitHigh,
+      low: fahrenheitLow
     }
+  }
 
-    setLocation(){
-        state = document.querySelector('#state').value.toLowerCase()
-        city = document.querySelector('#city').value.toLowerCase()
-        userWeather.makeCelsius()
-        userWeather.makeFahrenheit()
-        userWeather.getWeatherDescription()
+  async getWeather() {
+    let allData = await this.fetchData()
+    this.fahrenheit = this.makeFahrenheit(allData.main)
+    this.celsius = this.makeCelsius(allData.main)
+    this.weatherDescription = allData.weather[0].main 
+    return this
+  }
 
-    }
+  async setLocation() {
+    this.state = document.querySelector("#state").value.toLowerCase()
+    this.city = document.querySelector("#city").value.toLowerCase()
+  }
 }
 
-let userWeather = new GrabWeather
+let userWeather = new GrabWeather()
 
 let getWeatherButton = document.getElementById("getWeather")
 
-getWeatherButton.addEventListener('click', userWeather.setLocation);
+getWeatherButton.addEventListener("click", run.bind(userWeather))
 
+window.onload = ()=>{
+  run.call(userWeather)
+}
+
+function setScreenVars(weather) {
+  let elm_loc = document.querySelector('.location')
+  let elm_icon = document.querySelector('.icon')
+  let elm_high = document.querySelector('.high')
+  let elm_current = document.querySelector('.current')
+  let elm_low = document.querySelector('.low')
+  let elm_status = document.querySelector('.status')
+}
+
+function run() {
+  
+  userWeather.setLocation()
+  let weather = userWeather.getWeather()
+  setScreenVars(weather)
+}
